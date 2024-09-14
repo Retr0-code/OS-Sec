9974d1bd6c77de0be607bf1d79285e3a185ea733
Retr0-code <nikk3695@gmail.com>
2024-09-14 11:50:07 +0300

    Added comments and repeat option

--- pracs/2/win-thread-example.c
+++ pracs/2/win-thread-example.c
@@ -5,8 +5,10 @@
 
 #define THREADS_AMOUNT 5
 
+// Mutex for STDOUT resource
 static HANDLE stdout_mutex;
 
+// Structure for function procedure representing its arguments
 typedef struct
 {
 	uint32_t thread_id;
@@ -15,6 +17,7 @@ typedef struct
 
 void procedure(uint32_t thread_id, uint32_t millis)
 {
+	// Wait STDOUT mutex to unlock to write a message
 	DWORD wait = WaitForSingleObject(stdout_mutex, INFINITE);
 	switch (wait)
 	{
@@ -27,10 +30,12 @@ void procedure(uint32_t thread_id, uint32_t millis)
 		return;
 	}
 	
+	// Function pseudo-code
 	clock_t time_start = clock();
 	Sleep(millis);
 	clock_t time_end = clock();
 
+	// Wait STDOUT mutex to unlock to write a message
 	wait = WaitForSingleObject(stdout_mutex, INFINITE);
 	switch (wait)
 	{
@@ -46,6 +51,7 @@ void procedure(uint32_t thread_id, uint32_t millis)
 	ExitThread(ERROR_SUCCESS);
 }
 
+// Interface function for calling in separate threads
 void __stdcall thread_procedure(thread_args* args)
 {
 	procedure(args->thread_id, args->millis);
@@ -55,6 +61,7 @@ int main(int argc, char** argv)
 {
 	clock_t time_start = clock();
 
+	// Struct of arrays for new threads
 	typedef struct
 	{
 		HANDLE		threads_handle[THREADS_AMOUNT];
@@ -63,39 +70,51 @@ int main(int argc, char** argv)
 		thread_args	thread_arguments[THREADS_AMOUNT];
 	} threads_array;
 
+	// Get user input
 	printf("Enter %lu numbers representing time in milliseconds: ", THREADS_AMOUNT);
 	uint32_t threads_delay[THREADS_AMOUNT];
 	for (size_t thread_id = 0; thread_id < THREADS_AMOUNT; ++thread_id)
 		scanf_s("%ul", &threads_delay[thread_id]);
 
-	stdout_mutex = CreateMutex(NULL, FALSE, NULL);
-
-	threads_array threads;
-	for (uint32_t thread_id = 0; thread_id < THREADS_AMOUNT; ++thread_id)
+	char user_option = 'x';
+	do
 	{
-		threads.thread_arguments[thread_id] = (thread_args){ thread_id + 1, threads_delay[thread_id] };
-		threads.threads_handle[thread_id] = CreateThread(
-			NULL,
-			0,
-			thread_procedure,
-			&threads.thread_arguments[thread_id],
-			0,
-			&threads.threads_id[thread_id]
-		);
-	}
+		stdout_mutex = CreateMutex(NULL, FALSE, NULL);
 
-	WaitForMultipleObjects(THREADS_AMOUNT, threads.threads_handle, TRUE, INFINITE);
+		// Allocate new threads
+		threads_array threads;
+		for (uint32_t thread_id = 0; thread_id < THREADS_AMOUNT; ++thread_id)
+		{
+			threads.thread_arguments[thread_id] = (thread_args){ thread_id + 1, threads_delay[thread_id] };
+			threads.threads_handle[thread_id] = CreateThread(
+				NULL,
+				0,
+				thread_procedure,
+				&threads.thread_arguments[thread_id],
+				0,
+				&threads.threads_id[thread_id]
+			);
+		}
 
-	for (size_t thread_id = 0; thread_id < THREADS_AMOUNT; ++thread_id)
-	{
-		DWORD exit_code = 0;
-		GetExitCodeThread(threads.threads_handle[thread_id], &exit_code);
-		printf("Thread %lu (%lu) done with code %u\n", thread_id + 1, threads.threads_id[thread_id], exit_code);
-		CloseHandle(threads.threads_handle[thread_id]);
-	}
-	CloseHandle(stdout_mutex);
+		WaitForMultipleObjects(THREADS_AMOUNT, threads.threads_handle, TRUE, INFINITE);
 
+		// Close resources handles
+		for (size_t thread_id = 0; thread_id < THREADS_AMOUNT; ++thread_id)
+		{
+			DWORD exit_code = 0;
+			GetExitCodeThread(threads.threads_handle[thread_id], &exit_code);
+			printf("Thread %lu (%lu) done with code %u\n", thread_id + 1, threads.threads_id[thread_id], exit_code);
+			CloseHandle(threads.threads_handle[thread_id]);
+		}
+		CloseHandle(stdout_mutex);
+
+		puts("Enter 'x' to exit or 'r' to retry: ");
+		while (getchar() != '\n');
+
+	} while (getchar() == 'r');
 
 	clock_t time_end = clock();
+	printf("Program completed within %lu\n", time_end - time_start);
+
 	return 0;
 }
