41bb8fbb9c38d09a9d0525722f0a359675f83737
Retr0-code <nikk3695@gmail.com>
2024-11-23 11:31:50 +0300

    Export/Install Makefile script

--- lect/2/src/mysyslog_client.c
+++ lect/2/src/mysyslog_client.c
@@ -5,17 +5,47 @@
 #include <unistd.h>
 #include <getopt.h>
 
-#include "mysyslog.c"
+#include "mysyslog.h"
 
-void show_usage(void);
+typedef struct {
+    int         enum_val;
+    const char *str;
+} enum_str_pair;
 
-void show_options_args(const char *msg, const enum_str_pair *array, size_t options_len);
+enum_str_pair driver_so_filename_str[] = {
+    { drv_ascii,    "libmysyslog_text.so" },
+    { drv_json,     "libmysyslog_json.so" },
+};
+unsigned long int driver_so_filename_amount = sizeof(driver_so_filename_str) / sizeof(enum_str_pair);
+
+enum_str_pair level_str[] = {
+    { loglvl_DEBUG,     "DEBUG" },
+    { loglvl_INFO,      "INFO" },
+    { loglvl_WARN,      "WARN" },
+    { loglvl_ERROR,     "ERROR" },
+    { loglvl_CRITICAL,  "CRITICAL" }
+};
+unsigned long int levels_amount = sizeof(level_str) / sizeof(enum_str_pair);
+
+enum_str_pair format_str[] = {
+    { fmt_none, "" },
+    { fmt_txt,  ".txt" },
+    { fmt_log,  ".log" },
+    { fmt_json, ".json" },
+};
+unsigned long int formats_amount = sizeof(format_str) / sizeof(enum_str_pair);
 
 enum_str_pair driver_names[] = {
     { drv_ascii,    "ascii" },
     { drv_json,     "json" }
 };
 size_t driver_names_len = sizeof(driver_names) / sizeof(enum_str_pair);
+
+void show_usage(void);
+
+void show_options_args(const char *msg, const enum_str_pair *array, size_t options_len);
+
+int enum_from_str(const char *str, const enum_str_pair *array, size_t array_size);
 
 int main(int argc, char **argv) {
     int driver = drv_ascii;
@@ -109,4 +139,18 @@ void show_options_args(const char *msg, const enum_str_pair *array, size_t optio
     puts(msg);
     for (size_t i = 0; i < options_len; ++i)
         puts(array[i].str);
+}
+
+int enum_from_str(const char *str, const enum_str_pair *array, size_t array_size) {
+    if (str == NULL)
+        return -1;
+    
+    int enum_val = -1;
+    for (size_t i = 0; i < array_size; ++i) {
+        if (strcmp(array[i].str, str) == 0) {
+            enum_val = array[i].enum_val;
+            break;
+        }
+    }
+    return enum_val;
 }
